776ebdb46dd09f50b7ec6d03cefb45bb75753c5a
Gubi Zoltan Krisztofer <krisztofer.gubi@gmail.com>
2025-07-18 16:11:49 +0200

    treesitter added, lsp setup modified, blink.cmp further configured

--- lua/config/plugins/blinky.lua
+++ lua/config/plugins/blinky.lua
@@ -1,21 +1,54 @@
-return {
-  'saghen/blink.cmp',
-  dependencies = { 'rafamadriz/friendly-snippets' },
+return
+{
+	"saghen/blink.cmp",
+	version = "1.*",
+	dependencies = { "rafamadriz/friendly-snippets" },
+	opts = {
+		keymap = { preset = "default" },
+		appearance = {
+			use_nvim_cmp_as_default = true,
+			nerd_font_variant = "mono"
+		},
+		menu = {
+			auto_show = true,
+			draw = {
+				columns = {
+					{ "label",     "label_description", gap = 1 },
+					{ "kind_icon", "kind" }
+				},
+			}
+		},
+		documentation = {
+			auto_show = true,
+			auto_show_delay_ms = 500,
+		},
+		completion = {
+			documentation = {
+				auto_show = true,
+				auto_show_delay_ms = 500,
+			},
 
-  version = '1.*',
-  opts = {
- keymap = { preset = 'default' },
+			list = {
+				max_items = 200,
 
-    appearance = {
-	    use_nvim_cmp_as_default = true,
-	    nerd_font_variant = 'mono'
-    },
+				selection = {
+					preselect = true,
+					auto_insert = true,
+				},
 
-    sources = {
-      default = { 'lsp', 'path', 'snippets', 'buffer' },
-    },
+				cycle = {
+					from_bottom = true,
+					from_top = true,
+				},
+			},
 
-    fuzzy = { implementation = "prefer_rust_with_warning" }
-  },
-  opts_extend = { "sources.default" }
+		},
+
+
+		sources = {
+			default = { "lsp", "path", "snippets", "buffer" },
+		},
+		signature = { enabled = true },
+		fuzzy = { implementation = "prefer_rust_with_warning" }
+	}
 }
