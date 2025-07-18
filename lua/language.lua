baf669b80e1c546fa9bddb6044d13b40421104f6
Gubi Zoltan Krisztofer <krisztofer.gubi@gmail.com>
2025-07-18 14:19:53 +0200

    removed old unnecessary files

--- lua/language.lua
+++
@@ -1,6 +0,0 @@
-require("lspconfig").sqls.setup({
-  capabilities = require("cmp_nvim_lsp").default_capabilities(),
-   cmd = { "sqls", "-config", "sqls.yaml" },
-  root_dir = require("lspconfig.util").root_pattern("sqls.yaml", ".git")
-})
-
