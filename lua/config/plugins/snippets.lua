c709eabebc1d646df74abf9ccc1658b4dd98bd8e
Gubi Zoltan Krisztofer <krisztofer.gubi@gmail.com>
2025-07-21 21:45:12 +0200

    added python lsp and snippets

---
+++ lua/config/plugins/snippets.lua
@@ -0,0 +1,10 @@
+-- in your plugins.lua or plugins table
+return {
+	"L3MON4D3/LuaSnip",
+	dependencies = {
+		"rafamadriz/friendly-snippets",
+	},
+	config = function()
+		require("luasnip.loaders.from_vscode").lazy_load() -- loads friendly-snippets
+	end,
+}
